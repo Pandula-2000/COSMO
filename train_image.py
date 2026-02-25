9c013b5e064af5d0f0301831cabea23277e3c46f
Pandula Thennakoon <pandula66@gmail.com>
2026-02-25 04:41:18 +0000

    Refactor training script with argparse and model updates

--- train_image.py
+++ train_image.py
@@ -1,328 +1,237 @@
+import skimage
 import os
-import sys
-
-import time
-import numpy as np
-from skimage import color
-from tqdm import tqdm
-
-import cv2
 from PIL import Image
+import numpy as np
 import matplotlib.pyplot as plt
-
+import time
+import argparse
+import cv2
+from scipy import io
+from tqdm.auto import tqdm
 import torch
-import torch.nn as nn
-from torch.nn.utils import clip_grad_norm_
+from torch import nn
+import torch.nn.functional as F
 import torchvision.models as models
-from torch.optim.lr_scheduler import LambdaLR
-
-from modules.utils import normalize, measure, psnr
-
-from fractions import Fraction
+import torch.optim.lr_scheduler as lr_scheduler
+from modules import utils
+from modules import models
+!pip install -q pytorch-msssim # Install this library if error pops up (Uncomment and run)
+from pytorch_msssim import ssim
+from modules.models1 import INR
 
 torch.cuda.set_device(7)
 
+parser = argparse.ArgumentParser(description='INCODE')
 
+run_name = f"GPU7"
 image = "kodim20"
 
-input_img = f"data/Images/kodak/{image}.png"
+# Shared Parameters
+parser.add_argument('--input',type=str, default=f"data/Images/kodak/{image}.png", help='Input image path')
+parser.add_argument('--inr_model',type=str, default='COSMOV4', help='[gauss, mfn, relu, siren, wire, wire2d, ffn, incode]')
+parser.add_argument('--lr',type=float, default=9e-4, help='Learning rate')
+parser.add_argument('--using_schedular', type=bool, default=True, help='Whether to use schedular')
+parser.add_argument('--scheduler_b', type=float, default=0.1, help='Learning rate scheduler')
+parser.add_argument('--maxpoints', type=int, default=256*256, help='Batch size')
+parser.add_argument('--niters', type=int, default=4500, help='Number if iterations')
+parser.add_argument('--steps_til_summary', type=int, default=50, help='Number of steps till summary visualization')
 
-save_dir = "./Results/images"
-os.makedirs(save_dir, exist_ok=True)
+# INCODE Parameters
+parser.add_argument('--a_coef',type=float, default=0.1993, help='a coeficient')
+parser.add_argument('--b_coef',type=float, default=0.0196, help='b coeficient')
+parser.add_argument('--c_coef',type=float, default=0.0588, help='c coeficient')
+parser.add_argument('--d_coef',type=float, default=0.0269, help='d coeficient')
 
+args = parser.parse_args(args=[])
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+time_array = torch.zeros(args.niters, device=device)
 
-# Model configs
-niters = 500
-learning_rate = 0.01  # Learning rate
-decay_rate = 0.01 # Decay rate
+save_name = f"{run_name}_{args.inr_model}"
+save_path = f"Results/{save_name}"
+os.makedirs(save_path, exist_ok=True)
 
-act_params = 2
-in_features = 2
-out_features = 3
-hidden_layers = 3
-hidden_features = 256
-maxpoints = 256*256
-
-im = normalize(plt.imread(input_img).astype(np.float32), True)
+im_RGB_gt = utils.normalize(plt.imread(args.input).astype(np.float32), True)
+im = im_RGB_gt
 H, W, _ = im.shape
-if H > 2000 or W > 2000: scale = 1 / 2
-else: scale = 1
+print(f"Image size(H,W): ({H} x {W})")
 
-im= cv2.resize(im, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
+# --- Define Positional Encodings ---
+# Frequency Encoding
+pos_encode_freq = {'type':'frequency', 'use_nyquist': True, 'mapping_input': int(max(H, W)/3)}
+# Gaussian Encoding
+pos_encode_gaus = {'type':'gaussian', 'scale_B': 10, 'mapping_input': 256}
+# No Encoding
+pos_encode_no = {'type': None}
 
-# im_gt_RGB = im
+# --- Model Configurations ---
+print(f"Running Model: {args.inr_model}")
 
-H, W, _ = im.shape
-print(f"Image size: {H}x{W}")
-
-
-x, y = torch.linspace(-1, 1, W), torch.linspace(-1, 1, H)
-X, Y = torch.meshgrid(x, y, indexing='xy')
-coords = torch.hstack((X.reshape(-1, 1), Y.reshape(-1, 1)))[None, ...]
-
-gt = torch.tensor(im).cuda().reshape(H * W, 3)[None, ...]
-
-img = gt
-task = 'image'
-
-mse_array = torch.zeros(niters).cuda()
-time_array = torch.zeros_like(mse_array)
-consts, psnr_vals, step_array = [], [], []
-
-best_mse = torch.tensor(float('inf'))
-best_img = None
-
-rec = torch.zeros_like(gt)
-
-# IMPORTANT: Set T_range and C_range constraints according to the task.
-MLP_configs ={'task': task,
+act_prams=2
+hidden_lay=3
+MLP_configs={'task': 'image',
              'model': 'resnet34',
              'truncated_layer':5,
              'in_channels': 64,
-             'hidden_channels': [64, 32, act_params*(hidden_layers+1)],
+             #'hidden_channels': [64, 32, 4],
+             'hidden_channels': [64, 32, act_prams*(hidden_lay+1)],
              'mlp_bias':0.3120,
              'activation_layer': nn.SiLU,
-             'GT': torch.tensor(im).cuda()[None,...].permute(0, 3, 1, 2),
-             'T_range': [5, 7],
-             'c_range': [1, 2]
-              } # Run for new range
+             'GT': torch.tensor(im).to(device)[None,...].permute(0, 3, 1, 2),
+             'T_range': [0, 10],
+             'c_range': [0, 3],
+            }
 
+model =INR(args.inr_model).run(in_features=2,
+                                out_features=3,
+                                hidden_features=256,
+                                hidden_layers=3,
+                                #first_omega_0=30.0,
+                                #hidden_omega_0=30.0,
+                                #pos_encode_configs=pos_encode_freq,
+                                #ffn_type='relu',
+                                MLP_configs = MLP_configs
+                               ).to(device)
 
-class MLP(torch.nn.Sequential):
-    '''
-    Args:
-        in_channels (int): Number of input channels or features.
-        hidden_channels (list of int): List of hidden layer sizes. The last element is the output size.
-        mlp_bias (float): Value for initializing bias terms in linear layers.
-        activation_layer (torch.nn.Module, optional): Activation function applied between hidden layers. Default is SiLU.
-        bias (bool, optional): If True, the linear layers include bias terms. Default is True.
-        dropout (float, optional): Dropout probability applied after the last hidden layer. Default is 0.0 (no dropout).
-    '''
-    def __init__(self, MLP_configs, bias=True, dropout = 0.0):
-        super().__init__()
-        in_channels=MLP_configs['in_channels']
-        hidden_channels=MLP_configs['hidden_channels']
-        self.mlp_bias=MLP_configs['mlp_bias']
-        activation_layer=MLP_configs['activation_layer']
+# --- Training Loop ---
+init_time = time.time() #timearray setup
 
-        layers = []
-        in_dim = in_channels
-        for hidden_dim in hidden_channels[:-1]:
-            layers.append(torch.nn.Linear(in_dim, hidden_dim, bias=bias))
-            if MLP_configs['task'] == 'denoising':
-                layers.append(nn.LayerNorm(hidden_dim))
-            layers.append(activation_layer())
-            in_dim = hidden_dim
+# Optimizer setup
+if args.inr_model == 'wire' or  or args.inr_model == 'BandRC':
+    args.lr = args.lr * min(1, args.maxpoints / (H * W))
+optim = torch.optim.Adam(lr=args.lr, params=model.parameters())
+scheduler = lr_scheduler.LambdaLR(optim, lambda x: args.scheduler_b ** min(x / args.niters, 1))
 
-        layers.append(torch.nn.Linear(in_dim, hidden_channels[-1], bias=bias))
-        layers.append(torch.nn.Dropout(dropout))
+# Initialize lists for PSNR and MSE values
+psnr_values = []
+mse_array = torch.zeros(args.niters, device=device)
+# Initialize best loss value as positive infinity
+best_loss = torch.tensor(float('inf'))
+# Generate coordinate grid
+coords = utils.get_coords(H, W, dim=2)[None, ...]
+# Convert input image to a tensor and reshape
+gt = torch.tensor(im).reshape(H * W, 3)[None, ...].to(device)
+# Initialize a tensor for reconstructed data
+rec = torch.zeros_like(gt)
 
-        self.layers = nn.Sequential(*layers)
-        self.layers.apply(self.init_weights)
+for step in tqdm(range(args.niters)):
+    # Randomize the order of data points for each iteration
+    indices = torch.randperm(H*W)
 
-    def init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            nn.init.trunc_normal_(m.weight, std=0.001)
-            torch.nn.init.constant_(m.bias, self.mlp_bias)
+    # Process data points in batches
+    for b_idx in range(0, H*W, args.maxpoints):
+        b_indices = indices[b_idx:min(H*W, b_idx+args.maxpoints)]
+        b_coords = coords[:, b_indices, ...].to(device)
+        b_indices = b_indices.to(device)
 
-    def forward(self, x):
-        self.consts = self.layers(x)
-        return self.consts
+        # Calculate model output
+        if args.inr_model == 'incode' :
+            model_output, coef = model(b_coords)
+        else:
+            model_output = model(b_coords)
 
+        # Update the reconstructed data
+        with torch.no_grad():
+            rec[:, b_indices, :] = model_output
 
-class RaisedCosineImpulseResponseLayer(nn.Module):
-    def __init__(self, in_features, out_features, bias=True, is_first=False,
-                 beta0=0.5, eps=1e-8, out_real=False):
-        super().__init__()
+        # Calculate the output loss
+        output_loss = ((model_output - gt[:, b_indices, :])**2).mean()
 
-        self.beta0 = nn.Parameter(torch.tensor(beta0, dtype=torch.float), requires_grad=False)
-        self.eps = eps
-        self.is_first = is_first
-        self.out_real = out_real
+        if args.inr_model == 'incode':
+            # Calculate regularization loss for 'incode' model
+            a_coef, b_coef, c_coef, d_coef = coef[0]
+            reg_loss = args.a_coef * torch.relu(-a_coef) + \
+                       args.b_coef * torch.relu(-b_coef) + \
+                       args.c_coef * torch.relu(-c_coef) + \
+                       args.d_coef * torch.relu(-d_coef)
 
-        self.in_features = in_features
-        self.out_features = out_features
+            # Total loss for 'incode' model
+            loss = output_loss + reg_loss
+        else:
+            # Total loss for other models
+            loss = output_loss
 
-        dtype = torch.float if self.is_first else torch.cfloat
-        self.linear = nn.Linear(in_features, out_features, bias=bias, dtype=dtype)
-
-        nn.init.uniform_(self.linear.weight, -1/self.in_features, 1/self.in_features)  # Initialize weights (new method)
-
-    def forward(self, input, t0, c0):
-        input = input.to(next(self.parameters()).device)  # Move input to correct device
-        lin = self.linear(input)
-
-        if not self.is_first:
-            lin = lin / torch.abs(lin + self.eps)  # Normalize
-
-        f1 = (1 / t0) * torch.sinc(lin / t0) * torch.cos(torch.pi * self.beta0 * lin / t0)
-        f2 = 1 - (2 * self.beta0 * lin / t0) ** 2 + self.eps
-        theta = 2 * torch.pi * c0 * lin * 1j
-
-        rc = (f1 / f2)
-        out = rc * torch.exp(theta)
-
-
-        if not self.is_first:
-            out = out / torch.abs(out + self.eps)
-
-        return out.real if self.out_real else out
-
-class INR(nn.Module):
-    def __init__(self, in_features, hidden_features, hidden_layers, out_features):
-        super().__init__()
-
-        self.img = MLP_configs['GT'].cuda()
-
-        self.prior = MLP(MLP_configs)
-        self.T_range = MLP_configs['T_range']
-        self.c_range = MLP_configs['c_range']
-
-        model_ft = getattr(models, MLP_configs['model'])(pretrained=True)
-        self.feature_extractor = nn.Sequential(*list(model_ft.children())[:MLP_configs['truncated_layer']])
-
-        self.gap = nn.AdaptiveAvgPool1d(1)
-        self.nonlin = RaisedCosineImpulseResponseLayer
-
-        dtype = torch.cfloat
-
-        self.net = nn.ModuleList()  # Use ModuleList for proper CUDA movement
-        self.net.append(self.nonlin(in_features, hidden_features, is_first=True))
-        for _ in range(hidden_layers):
-            self.net.append(self.nonlin(hidden_features, hidden_features))
-
-        self.final_linear = nn.Linear(hidden_features, out_features, dtype=dtype)
-
-    def forward(self, coords):
-        extracted_features = self.feature_extractor(self.img)
-
-        gap = self.gap(extracted_features.view(extracted_features.size(0), extracted_features.size(1), -1))
-        coef = self.prior(gap[..., 0]).view(4,2)
-        t0, c0 = torch.unbind(coef, dim=1)
-
-        # ------------------------------- Sigmoid Projection ---------------------------
-        t0 = torch.sigmoid(t0) * (self.T_range[1] - self.T_range[0]) + self.T_range[0]
-        c0 = torch.sigmoid(c0) * (self.c_range[1] - self.c_range[0]) + self.c_range[0]
-        # ------------------------------------------------------------------------------
-
-        output = coords
-        for id, lyr in enumerate(self.net):
-            output = lyr(output, t0[id], c0[id])
-        output = self.final_linear(output).real
-
-        return nn.Sigmoid()(output)
-
-
-
-
-model = INR(
-    in_features=in_features,
-    out_features=out_features,
-    hidden_features=hidden_features,
-    hidden_layers=hidden_layers)
-
-model.cuda()
-
-optim = torch.optim.Adam(lr=learning_rate * min(1, maxpoints / (H * W)),
-                         params=model.parameters())
-
-scheduler = LambdaLR(optim, lambda x: decay_rate ** min(x / niters, 1))
-tot_iters = 0
-
-
-
-print(f"Total Parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
-
-
-
-def MSE_loss(x, y): return ((x - y) ** 2).mean()
-
-init_time = time.time()
-tbar = tqdm(range(niters))
-
-# -------------------------------- Training Loop ---------------------------------
-for epoch in (tbar):
-    indices = torch.randperm(H * W)
-    for b_idx in range(0, H*W, maxpoints):
-        b_indices = indices[b_idx:min(H*W, b_idx+maxpoints)]
-        b_coords = coords[:, b_indices, ...].cuda()
-        b_indices = b_indices.cuda()
-
-        pixelvalues = model(b_coords)
-        with torch.no_grad(): rec[:, b_indices, :] = pixelvalues
-        loss = MSE_loss(pixelvalues, img[:, b_indices, :])
-
+        # Perform backpropagation and update model parameters
         optim.zero_grad()
         loss.backward()
-        clip_grad_norm_(model.parameters(), 1.0)
         optim.step()
 
-    time_array[epoch] = time.time() - init_time
 
+    time_array[step] = time.time() - init_time #timearray setup (step )
+    # Calculate PSNR
     with torch.no_grad():
-        mse_array[epoch] = ((gt - rec) ** 2).mean().item()
+        mse_array[step] = ((gt - rec)**2).mean().item()
+        psnr = -10*torch.log10(mse_array[step])
+        psnr_values.append(psnr.item())
 
-        im_gt = gt.reshape(H, W, 3).permute(2, 0, 1)[None, ...]
-        im_rec = rec.reshape(H, W, 3).permute(2, 0, 1)[None, ...]
+    # Adjust learning rate using a scheduler if applicable
+    if args.using_schedular:
+        if args.inr_model == 'incode' and 30 < step:
+            scheduler.step()
+        else:
+            scheduler.step()
 
-        consts.append(model.prior.consts.cpu().detach().numpy())
-
-        psnrval = -10 * torch.log10(mse_array[epoch])
-        psnr_vals.append(psnrval.cpu().numpy())
-
-        tbar.set_description(f"Loss: {loss.item():.6f} "
-                     f"| PSNR: {psnrval:.2f}")
-        tbar.refresh()
-
-    scheduler.step()
+    # Prepare reconstructed image for visualization
     imrec = rec[0, ...].reshape(H, W, 3).detach().cpu().numpy()
 
-    if (mse_array[epoch] < best_mse) or (epoch == 0):
-        best_mse = mse_array[epoch]
+    # Check if the current iteration's loss is the best so far
+    if (mse_array[step] < best_loss) or (step == 0):
+        best_loss = mse_array[step]
+        best_flat_img = rec
         best_img = imrec
-        best_epoch = epoch
-        best_model_state = model.state_dict()
+        # best_img = (best_img - best_img.min()) / (best_img.max() - best_img.min())
+        best_epoch=step
 
-    tot_iters+=1
+    # Display intermediate results at specified intervals
+    if step % args.steps_til_summary == 0:
+        print("Epoch: {} | Total Loss: {:.5f} | PSNR: {:.4f}".format(step,
+                                                                     mse_array[step].item(),
+                                                                     psnr.item()))
 
+print(f"Final PSNR: {psnr_values[-1]}")
 
+def get_np_psnr(image1, image2):
+  loss = ((image1.astype(np.float32) - image2.astype(np.float32))**2).mean()
+  return -10*np.log10(loss)
 
-best_img = best_img.astype(np.float32)
-im = im.astype(np.float32) # Ensure float32 data type
+def get_np_loss(image1, image2):
+  loss = ((image1.astype(np.float32) - image2.astype(np.float32))**2).mean()
+  return loss
 
-work_dir = f"{save_dir}/{image}"
-os.makedirs(work_dir, exist_ok=True)
+rec_loss = get_np_loss(best_img, im)
+rec_psnr = get_np_psnr(best_img, im)
+print(rec_loss, rec_psnr)
 
-weight_path = os.path.join(work_dir, f"{image}_iters_{tot_iters}_psnr_{max(psnr_vals):.2f}.pth")
-torch.save({'state_dict': best_model_state,
+weight_path = os.path.join(save_path, f"iters_{len(psnr_values)}_psnr_{max(psnr_values):.2f}.pth")
+torch.save({'state_dict': model.state_dict(),
             'best_epoch': best_epoch,
-            'gt': im,
+            'gt': im_RGB_gt,
             'rec': best_img,
-            'consts_array': np.array(consts),
+            #'consts_array': np.array(consts),
             'time_array': time_array.detach().cpu().numpy(),
             'mse_array': mse_array.detach().cpu().numpy(),
-            'psnr_vals': np.array(psnr_vals)
+            'psnr_vals_hsv_domain': np.array(psnr_values)
             }, weight_path)
 
-
-fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-
-# Display the first image
-axes[0].imshow(im)
-#axes[0].imshow(im)
+# Plot
+fig, axes = plt.subplots(1, 2, figsize=(10, 10))
 axes[0].set_title('Ground Truth')
-axes[0].axis('off')  # Hide axis
-
-# Display the second image
+axes[0].imshow(im_RGB_gt)
+axes[0].axis('off')
+axes[1].set_title(f"RGB_PSNR= {rec_psnr:.3f} ")
 axes[1].imshow(best_img)
-#axes[1].imshow(best_img)
-axes[1].set_title(f"Reconstructed Image: PSNR {max(psnr_vals):.4f}")#n RGB PSNR {(rgb_psnr):.4f}")
-axes[1].axis('off')  # Hide axis
+axes[1].axis('off')
 
-# Save figs
-plt.savefig(f"{work_dir}/{image}_iters_{tot_iters}_psnr_{max(psnr_vals):.2f}_comparison.png")
+plt.savefig(f"{save_path}/comparison.png")
+# iters_{len(psnr_values)}_psnr_{max(psnr_values):.2f}_
 
-plt.close()
+plt.show()
+
+
+# Print maximum PSNR achieved during training
+print('--------------------')
+print('Max PSNR:', max(psnr_values))
+print('--------------------')
+print(best_epoch)
 
 
 def get_sigmoid_projected_params(A, T_range=[0,10], c_range=[0,3]):
